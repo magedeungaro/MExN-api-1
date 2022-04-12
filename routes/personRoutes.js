8d7f8715b8b5a4cfae8510dc718fb1f2e39051dc
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-12 20:39:12 -0300

    update person route

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -40,7 +40,27 @@ router.get('/:id', async (req, res) => {
   try {
     const person = await Person.findOne({ _id: id })
 
-    if (!person) return res.status(422).json({ message: 'User not found' })
+    if (!person) return res.status(422).json({ message: 'user not found' })
+
+    res.status(200).json(person)
+  }
+  catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+router.patch('/:id', async (req, res) => {
+  const id = req.params.id
+  const { name, salary, approved } = req.body
+  const person = {
+    name,
+    salary,
+    approved
+  }
+  try {
+    const updatedPerson = await Person.updateOne({ _id: id }, person)
+
+    if (updatedPerson.matchedCount === 0) return res.status(422).json({ message: 'user not found' })
 
     res.status(200).json(person)
   }
